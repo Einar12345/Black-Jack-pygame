479e2d26b875cd75d3342565d90124c14a8870d3
Einar <isak.einarsson04@skola.stenungsund.se>
2023-04-01 02:50:53 +0200

    23-04-01
    ny kod

--- Black-Jack-pygame.py
+++ Black-Jack-pygame.py
@@ -47,26 +47,13 @@ text8 = textfont.render('Player:' , True , color)
 text9 = textfont.render('Dealer:' , True , color)
 
 
-skort=""
-skort2=""
-skort3=""
-skort4=""
-skort5=""
-skort6=""
-dkort=""
-dkort2=""
-dkort3=""
-dkort4=""
-dkort5=""
-dkort6=""
-
 spelare = 0
+spelare_lista = []
 dealer = 0
+dealer_lista = []
 
 sy=100
 dy=450
-sx=100
-dx=100
 
 hit=-1
 stand=-1
@@ -75,19 +62,7 @@ reset=0
 reset1=0
 reset2=0
 reset3=0
-
-skortD=0
-skort2D=0
-skort3D=0
-skort4D=0
-skort5D=0
-skort6D=0
-dkortD=0
-dkort2D=0
-dkort3D=0
-dkort4D=0
-dkort5D=0
-dkort6D=0
+reset4=0
 
 pengar=100
 bid=0
@@ -121,11 +96,25 @@ while True:
                 if bid > 1:
                     if hit < 4:
                         if stand == 0:
-                            hit+=1
+                            # hit+=1
+                            kort = random.choice(kortlek)
+                            kortlek.remove(kort)
+                            spelare_lista.append(kort)
+                            spelare += int(kort[1:])
+                            if kort[1:] == "11":
+                                spelare += -1
+                            if kort[1:] == "12":
+                                spelare += -2
+                            if kort[1:] == "13":
+                                spelare += -3
+                            if kort[1:] == "1":
+                                if spelare < 12:
+                                    spelare += 10
 
             if width/3 <= mouse[0] <= width/3+140 and height-45 <= mouse[1] <= height-5: # Stand
                 if bid > 1:
                 	if stand == 0:
+                            dealer_lista.remove(blank)
                             stand=1
 
             if width/3*2 <= mouse[0] <= width/3*2+140 and height-45 <= mouse[1] <= height-5: # Double
@@ -143,36 +132,15 @@ while True:
                     dealer = 0
                     hit=-1
                     stand=-1
-                    reset=0
-                    reset1=0
-                    skort=""
-                    skort2=""
-                    skort3=""
-                    skort4=""
-                    skort5=""
-                    skort6=""
-                    dkort=""
-                    dkort2=""
-                    dkort3=""
-                    dkort4=""
-                    dkort5=""
-                    dkort6=""
-                    skortD=0
-                    skort2D=0
-                    skort3D=0
-                    skort4D=0
-                    skort5D=0
-                    skort6D=0
-                    dkortD=0
-                    dkort2D=0
-                    dkort3D=0
-                    dkort4D=0
-                    dkort5D=0
-                    dkort6D=0
                     bid=0
                     runda+=1
+                    reset=0
+                    reset1=0
                     reset2=0
                     reset3=0
+                    reset4=0
+                    spelare_lista=[]
+                    dealer_lista=[]
                     # print("Reset", kortlek, len(kortlek))
 
             if hit == -1:
@@ -193,24 +161,31 @@ while True:
 
 
 
+    # Bakgrund
     window.blit(bg_img, [0, 0])
 
-    # All text
+    # Visa kortens värde
     if stand >= 0:
         window.blit(bluechip, [1632, 125])
         spoints = bigfont.render(str(spelare), True, color)
         window.blit(spoints, [1700, 193])
-    # dpoints = bigfont.render(str(dealer), True, color)
-    # window.blit(dpoints, [1700, 600])
-    window.blit(text6, [100, 750])
-    window.blit(text7, [400, 750])
+        window.blit(redchip, [1632, 475])
+        dpoints = bigfont.render(str(dealer), True, color)
+        window.blit(dpoints, [1700, 543])
+
+    # Visa pengar och bid
+    window.blit(text6, [100, 750]) # Money:
+    window.blit(text7, [400, 750]) # Bid:
     pengarD = bigfont.render(str(pengar), True, color)
     window.blit(pengarD, [100, 850])
     bidD = bigfont.render(str(bid), True, color)
     window.blit(bidD, [400, 850])
+
     if hit >= 0:
-        window.blit(text8, [100, 50])
-        window.blit(text9, [100, 400])
+        window.blit(text8, [100, 50]) # Player:
+        window.blit(text9, [100, 400]) # Dealer:
+
+    # Visa antal rundor
     rundortext = textfont.render("Runda:", True, color)
     window.blit(rundortext, [1700, 75])
     rundor = textfont.render(str(runda), True, color)
@@ -271,368 +246,80 @@ while True:
             window.blit(text555 , (width/3+20,height-245))
 
 
-    while skort == "":
-        skort = random.choice(kortlek)
-        kortlek.remove(skort)
-        skort_ = pygame.image.load(kortlek_map[skort]).convert_alpha()
-
-    while skort2 == "":
-        skort2 = random.choice(kortlek)
-        kortlek.remove(skort2)
-        skort2_ = pygame.image.load(kortlek_map[skort2]).convert_alpha()
-
-    while skort3 == "":
-    	skort3 = random.choice(kortlek)
-    	kortlek.remove(skort3)
-    	skort3_ = pygame.image.load(kortlek_map[skort3]).convert_alpha()
-
-    while skort4 == "":
-    	skort4 = random.choice(kortlek)
-    	kortlek.remove(skort4)
-    	skort4_ = pygame.image.load(kortlek_map[skort4]).convert_alpha()
-
-    while skort5 == "":
-    	skort5 = random.choice(kortlek)
-    	kortlek.remove(skort5)
-    	skort5_ = pygame.image.load(kortlek_map[skort5]).convert_alpha()
-
-    while skort6 == "":
-        skort6 = random.choice(kortlek)
-        kortlek.remove(skort6)
-        skort6_ = pygame.image.load(kortlek_map[skort6]).convert_alpha()
-
-
-    if hit == 0:
-        sx=100
-        window.blit(skort_, [sx, sy])
-        if skortD == 0:
-            spelare += int(skort[1:])
-            if skort[1:] == "11":
+    # Spelare startkort
+    if reset2 == 0:
+        for startkort in range(2):
+            kort = random.choice(kortlek)
+            kortlek.remove(kort)
+            spelare_lista.append(kort)
+            spelare += int(kort[1:])
+            if kort[1:] == "11":
                 spelare += -1
-            if skort[1:] == "12":
+            if kort[1:] == "12":
                 spelare += -2
-            if skort[1:] == "13":
+            if kort[1:] == "13":
                 spelare += -3
-            if skort[1:] == "1":
-                spelare += 10
-            skortD=1
-        sx+=250
-        window.blit(skort2_, [sx, sy])
-        if skort2D == 0:
-            spelare += int(skort2[1:])
-            if skort2[1:] == "11":
-                spelare += -1
-            if skort2[1:] == "12":
-                spelare += -2
-            if skort2[1:] == "13":
-                spelare += -3
-            if skort2[1:] == "1":
+            if kort[1:] == "1":
                 if spelare < 12:
                     spelare += 10
-            skort2D=1
-    if hit == 1:
-        sx=100
-        window.blit(skort_, [sx, sy])
-        sx+=250
-        window.blit(skort2_, [sx, sy])
-        sx+=250
-        window.blit(skort3_, [sx, sy])
-        if skort3D == 0:
-            spelare += int(skort3[1:])
-            if skort3[1:] == "11":
-                spelare += -1
-            if skort3[1:] == "12":
-                spelare += -2
-            if skort3[1:] == "13":
-                spelare += -3
-            if skort3[1:] == "1":
-                if spelare < 12:
-                    spelare += 10
-            skort3D=1
-    if hit == 2:
-        sx=100
-        window.blit(skort_, [sx, sy])
-        sx+=250
-        window.blit(skort2_, [sx, sy])
-        sx+=250
-        window.blit(skort3_, [sx, sy])
-        sx+=250
-        window.blit(skort4_, [sx, sy])
-        if skort4D == 0:
-            spelare += int(skort4[1:])
-            if skort4[1:] == "11":
-                spelare += -1
-            if skort4[1:] == "12":
-                spelare += -2
-            if skort4[1:] == "13":
-                spelare += -3
-            if skort4[1:] == "1":
-                if spelare < 12:
-                    spelare += 10
-            skort4D=1
-    if hit == 3:
-        sx=100
-        window.blit(skort_, [sx, sy])
-        sx+=250
-        window.blit(skort2_, [sx, sy])
-        sx+=250
-        window.blit(skort3_, [sx, sy])
-        sx+=250
-        window.blit(skort4_, [sx, sy])
-        sx+=250
-        window.blit(skort5_, [sx, sy])
-        if skort5D == 0:
-            spelare += int(skort5[1:])
-            if skort5[1:] == "11":
-                spelare += -1
-            if skort5[1:] == "12":
-                spelare += -2
-            if skort5[1:] == "13":
-                spelare += -3
-            if skort5[1:] == "1":
-                if spelare < 12:
-                    spelare += 10
-            skort5D=1
-    if hit == 4:
-        sx=100
-        window.blit(skort_, [sx, sy])
-        sx+=250
-        window.blit(skort2_, [sx, sy])
-        sx+=250
-        window.blit(skort3_, [sx, sy])
-        sx+=250
-        window.blit(skort4_, [sx, sy])
-        sx+=250
-        window.blit(skort5_, [sx, sy])
-        sx+=250
-        window.blit(skort6_, [sx, sy])
-        if skort6D == 0:
-            spelare += int(skort6[1:])
-            if skort6[1:] == "11":
-                spelare += -1
-            if skort6[1:] == "12":
-                spelare += -2
-            if skort6[1:] == "13":
-                spelare += -3
-            if skort6[1:] == "1":
-                if spelare < 12:
-                    spelare += 10
-            skort6D=1
+        reset2=1
+
+    # Printar spelarens kort
+    if hit >= 0:
+        for index in range(len(spelare_lista)):
+            kort_load = pygame.image.load(kortlek_map[spelare_lista[index]]).convert_alpha()
+            window.blit(kort_load, [100 + 250*index, sy])
 
 
-    while dkort == "":
-    	dkort = random.choice(kortlek)
-    	kortlek.remove(dkort)
-    	dkort_ = pygame.image.load(kortlek_map[dkort]).convert_alpha()
 
-    while dkort2 == "":
-    	dkort2 = random.choice(kortlek)
-    	kortlek.remove(dkort2)
-    	dkort2_ = pygame.image.load(kortlek_map[dkort2]).convert_alpha()
-
-    while dkort3 == "":
-    	dkort3 = random.choice(kortlek)
-    	kortlek.remove(dkort3)
-    	dkort3_ = pygame.image.load(kortlek_map[dkort3]).convert_alpha()
-
-    while dkort4 == "":
-    	dkort4 = random.choice(kortlek)
-    	kortlek.remove(dkort4)
-    	dkort4_ = pygame.image.load(kortlek_map[dkort4]).convert_alpha()
-
-    while dkort5 == "":
-    	dkort5 = random.choice(kortlek)
-    	kortlek.remove(dkort5)
-    	dkort5_ = pygame.image.load(kortlek_map[dkort5]).convert_alpha()
-
-    while dkort6 == "":
-        dkort6 = random.choice(kortlek)
-        kortlek.remove(dkort6)
-        dkort6_ = pygame.image.load(kortlek_map[dkort6]).convert_alpha()
-
+    # Dealer startkort
     if stand == 0:
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        if dkortD == 0:
-            dealer += int(dkort[1:])
-            if dkort[1:] == "11":
+        if reset4 == 0:
+            kort2 = random.choice(kortlek)
+            kortlek.remove(kort2)
+            dealer_lista.append(kort2)
+            dealer += int(kort2[1:])
+            if kort2[1:] == "11":
                 dealer += -1
-            if dkort[1:] == "12":
+            if kort2[1:] == "12":
                 dealer += -2
-            if dkort[1:] == "13":
+            if kort2[1:] == "13":
                 dealer += -3
-            if dkort[1:] == "1":
-                dealer += 10
-            dkortD=1
-        dx+=250
-        window.blit(blank, [dx, dy])
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
+            if kort2[1:] == "1":
+                if dealer < 12:
+                    dealer += 10
+            dealer_lista.append(blank)
+            reset4=1
+        kort2_ = pygame.image.load(kortlek_map[kort2]).convert_alpha()
+        window.blit(kort2_, [100, dy])
+        window.blit(blank, [350, dy])
 
-    if stand == 1:
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        dx+=250
-        window.blit(dkort2_, [dx, dy])
-        if dkort2D == 0:
-            dealer += int(dkort2[1:])
-            if dkort2[1:] == "11":
+    # Dealer dra kort
+    if (stand == 1) or (reset1 == 1):
+        if dealer < 17:
+            kort2 = random.choice(kortlek)
+            kortlek.remove(kort2)
+            dealer_lista.append(kort2)
+            dealer += int(kort2[1:])
+            if kort2[1:] == "11":
                 dealer += -1
-            if dkort2[1:] == "12":
+            if kort2[1:] == "12":
                 dealer += -2
-            if dkort2[1:] == "13":
+            if kort2[1:] == "13":
                 dealer += -3
-            if dkort2[1:] == "1":
+            if kort2[1:] == "1":
                 if dealer < 12:
                     dealer += 10
-            dkort2D=1
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
-        pygame.display.update()
-        if  dealer > spelare:
-            stand=1
-            reset=1
-        elif dealer > 16:
-            stand=1
-            reset=1
-        else:
-            time.sleep(1)
-            stand=2
-    if stand == 2:
-        pygame.display.update()
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        dx+=250
-        window.blit(dkort2_, [dx, dy])
-        dx+=250
-        window.blit(dkort3_, [dx, dy])
-        if dkort3D == 0:
-            dealer += int(dkort3[1:])
-            if dkort3[1:] == "11":
-                dealer += -1
-            if dkort3[1:] == "12":
-                dealer += -2
-            if dkort3[1:] == "13":
-                dealer += -3
-            if dkort3[1:] == "1":
-                if dealer < 12:
-                    dealer += 10
-            dkort3D=1
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
-        pygame.display.update()
-        if  dealer > spelare:
-            stand=2
-            reset=1
-        elif dealer > 16:
-            stand=2
-            reset=1
-        else:
-            time.sleep(1)
-            stand=3
-    if stand == 3:
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        dx+=250
-        window.blit(dkort2_, [dx, dy])
-        dx+=250
-        window.blit(dkort3_, [dx, dy])
-        dx+=250
-        window.blit(dkort4_, [dx, dy])
-        if dkort4D == 0:
-            dealer += int(dkort4[1:])
-            if dkort4[1:] == "11":
-                dealer += -1
-            if dkort4[1:] == "12":
-                dealer += -2
-            if dkort4[1:] == "13":
-                dealer += -3
-            if dkort4[1:] == "1":
-                if dealer < 12:
-                    dealer += 10
-            dkort4D=1
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
-        pygame.display.update()
-        if  dealer > spelare:
-            stand=3
-            reset=1
-        elif dealer > 16:
-            stand=3
-            reset=1
-        else:
-            time.sleep(1)
-            stand=4
-    if stand == 4:
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        dx+=250
-        window.blit(dkort2_, [dx, dy])
-        dx+=250
-        window.blit(dkort3_, [dx, dy])
-        dx+=250
-        window.blit(dkort4_, [dx, dy])
-        dx+=250
-        window.blit(dkort5_, [dx, dy])
-        if dkort5D == 0:
-            dealer += int(dkort5[1:])
-            if dkort5[1:] == "11":
-                dealer += -1
-            if dkort5[1:] == "12":
-                dealer += -2
-            if dkort5[1:] == "13":
-                dealer += -3
-            if dkort5[1:] == "1":
-                if dealer < 12:
-                    dealer += 10
-            dkort5D=1
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
-        pygame.display.update()
-        if  dealer > spelare:
-            stand=4
-            reset=1
-        elif dealer > 16:
-            stand=4
-            reset=1
-        else:
-            time.sleep(1)
-            stand=5
-    if stand == 5:
-        dx=100
-        window.blit(dkort_, [dx, dy])
-        dx+=250
-        window.blit(dkort2_, [dx, dy])
-        dx+=250
-        window.blit(dkort3_, [dx, dy])
-        dx+=250
-        window.blit(dkort4_, [dx, dy])
-        dx+=250
-        window.blit(dkort5_, [dx, dy])
-        dx+=250
-        window.blit(dkort6_, [dx, dy])
-        if dkort6D == 0:
-            dealer += int(dkort6[1:])
-            if dkort6[1:] == "11":
-                dealer += -1
-            if dkort6[1:] == "12":
-                dealer += -2
-            if dkort6[1:] == "13":
-                dealer += -3
-            if dkort6[1:] == "1":
-                if dealer < 12:
-                    dealer += 10
-            dkort6D=1
-        dpoints = bigfont.render(str(dealer), True, color)
-        window.blit(redchip, [1632, 475])
-        window.blit(dpoints, [1700, 543])
+
+        # Printar dealerns kort
+        for index in range(len(dealer_lista)):
+            kort_load2 = pygame.image.load(kortlek_map[dealer_lista[index]]).convert_alpha()
+            window.blit(kort_load2, [100 + 250*index, dy])
+
+    if  dealer > spelare:
         reset=1
-
+    elif dealer > 16:
+        reset=1
 
     # Skapar ny kortlek om kortleken är tom
     if len(kortlek) < 13:
@@ -645,24 +332,25 @@ while True:
     if spelare > 20:
         if stand == 0:
             if reset1 == 0:
+                dealer_lista.remove(blank)
                 stand=1
                 reset1=1
 
     # Vinstodds
     if reset == 1:
         if reset3 == 0:
-            if spelare == 21 and dealer != 21:
-                if hit == 0:
-                    if (skort[1:] == "1") or (skort2[1:] == "1"):
-                        pengar+=bid
-                        bid=bid*2
-                        pengar+=bid
-                        reset3=1
-                else:
-                    bid=bid*2
-                    pengar+=bid
-                    reset3=1
-            elif spelare > 21:
+            # if spelare == 21 and dealer != 21:
+            #     if hit == 0:
+            #         if (skort[1:] == "1") or (skort2[1:] == "1"):
+            #             pengar+=bid
+            #             bid=bid*2
+            #             pengar+=bid
+            #             reset3=1
+            #     else:
+            #         bid=bid*2
+            #         pengar+=bid
+            #         reset3=1
+            if spelare > 21:
                 reset3=1
             elif (spelare == dealer):
                 pengar+=bid
